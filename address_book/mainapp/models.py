921a1dc35ce9ca42cad753c42f86a17b54d82792
Vladislav <vladislav.sytyuk@gmail.com>
2021-11-16 19:48:21 +0200

    Create models (Contact, Address)

--- address_book/mainapp/models.py
+++ address_book/mainapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(blank=False, max_length=150)
+    last_name = models.CharField(blank=False, max_length=150)
+    phone_number = models.CharField(blank=False, max_length=20)
+    photo = models.ImageField(blank=True)
+    url = models.URLField(blank=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['first_name', 'last_name'], name='unique_full_name')
+        ]
+
+
+class Address(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='address')
+    country = models.CharField(blank=True, max_length=100)
+    city = models.CharField(blank=True, max_length=100)
+    street = models.CharField(blank=True, max_length=300)
