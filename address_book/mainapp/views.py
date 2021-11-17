1f35cf6d8ab69a04015494f91bfb85e37f8961ec
Vladislav <vladislav.sytyuk@gmail.com>
2021-11-17 12:42:32 +0200

    Add ContactListView, ContactDetailView

--- address_book/mainapp/views.py
+++ address_book/mainapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Contact, Address
+
+
+class ContactListView(ListView):
+    model = Contact
+    template_name = 'mainapp/base.html'
+    context_object_name = 'contacts'
+
+    def get_queryset(self):
+        return Contact.objects.all().order_by('first_name', 'last_name')
+
+
+class ContactDetailView(DetailView):
+    model = Contact
+    template_name = 'mainapp/contact_detail.html'
+    context_object_name = 'contact'
+    slug_url_kwarg = 'slug'
+
