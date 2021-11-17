1e39517f8e6f1c4affcce198073c041627839b31
Vladislav <vladislav.sytyuk@gmail.com>
2021-11-17 19:52:47 +0200

    Add urlpatterns for contact_detail, remove contact

--- address_book/mainapp/urls.py
+++ address_book/mainapp/urls.py
@@ -5,5 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', ContactListView.as_view(), name='base'),
-    path('contact/<slug:slug>/', ContactDetailView.as_view(), name='contact_detail')
+    path('contact/<pk>/', ContactDetailView.as_view(), name='contact_detail'),
+    path('add-contact/', AddContactView.as_view(), name='add_contact'),
+    path('delete/<int:pk>/', DeleteContact.as_view(), name='delete_contact')
 ]
