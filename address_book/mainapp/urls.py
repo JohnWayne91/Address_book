9ab17b135bde3fb60b8ee0aabcde725a3092c3f8
Vladislav <vladislav.sytyuk@gmail.com>
2021-11-17 21:11:47 +0200

    Add urlpattern for updating contacts

--- address_book/mainapp/urls.py
+++ address_book/mainapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', ContactListView.as_view(), name='base'),
     path('contact/<pk>/', ContactDetailView.as_view(), name='contact_detail'),
     path('add-contact/', AddContactView.as_view(), name='add_contact'),
-    path('delete/<int:pk>/', DeleteContact.as_view(), name='delete_contact')
+    path('delete/<int:pk>/', DeleteContact.as_view(), name='delete_contact'),
+    path('update/<int:pk>/edit', ContactUpdateView.as_view(), name='update_contact')
 ]
