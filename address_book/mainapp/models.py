0ac6640fcc105db5d22d257883e74924494aeee9
Vladislav <vladislav.sytyuk@gmail.com>
2021-11-17 12:38:41 +0200

    Change models architecture

--- address_book/mainapp/models.py
+++ address_book/mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Contact(models.Model):
@@ -7,6 +8,7 @@ class Contact(models.Model):
     phone_number = models.CharField(blank=False, max_length=20)
     photo = models.ImageField(blank=True)
     url = models.URLField(blank=True)
+    address = models.ForeignKey('Address', blank=True, null=True, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True)
 
     class Meta:
@@ -14,9 +16,14 @@ class Contact(models.Model):
             models.UniqueConstraint(fields=['first_name', 'last_name'], name='unique_full_name')
         ]
 
+    def get_absolute_url(self):
+        return reverse('contact_detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Address(models.Model):
-    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='address')
     country = models.CharField(blank=True, max_length=100)
     city = models.CharField(blank=True, max_length=100)
     street = models.CharField(blank=True, max_length=300)
