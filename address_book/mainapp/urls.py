a979a99d068d61f34298d575a90cf9696deac486
Vladislav <vladislav.sytyuk@gmail.com>
2021-11-18 12:05:05 +0200

    Add search_result to urlpatterns

--- address_book/mainapp/urls.py
+++ address_book/mainapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contact/<pk>/', ContactDetailView.as_view(), name='contact_detail'),
     path('add-contact/', AddContactView.as_view(), name='add_contact'),
     path('delete/<int:pk>/', DeleteContact.as_view(), name='delete_contact'),
-    path('update/<int:pk>/edit', ContactUpdateView.as_view(), name='update_contact')
+    path('update/<int:pk>/edit', ContactUpdateView.as_view(), name='update_contact'),
+    path('search/', SearchResultsView.as_view(), name='search_results')
 ]
